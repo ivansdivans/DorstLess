ac735c8ef826b6b18c403360bafce402bacf2beb
Ivans Mihailovs <imi@mediaan.com>
2023-08-25 18:43:26 +0200

    Add badge code

---
+++ DorstLess/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  DorstLess
+//
+//  Created by Ivans Mihailovs on 25/08/2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
