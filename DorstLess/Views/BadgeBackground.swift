4b5262205e493ca96b6853f006ff692fe31ddc70
Ivans Mihailovs <imi@mediaan.com>
2023-08-25 12:39:07 +0200

    Change gradient color

--- DorstLess/Views/BadgeBackground.swift
+++ DorstLess/Views/BadgeBackground.swift
@@ -52,7 +52,7 @@ struct BadgeBackground: View {
         }
         .aspectRatio(1, contentMode: .fit)
     }
-    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientStart = Color(red: 0.0 / 255, green: 191.0 / 255, blue: 255.0 / 255)
     static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
 
